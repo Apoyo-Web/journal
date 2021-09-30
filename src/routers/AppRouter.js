0f522d6abf775f8b4030913b0ee634b16200956a
Apoyo-Web <mpienso@gmail.com>
2021-09-30 16:47:57 +0200

    Loading...

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 
 import { JournalScreen } from '../componets/journal/JournalScreen'
+import ReactLoading from 'react-loading'
 
 import { AuthRouter } from './AuthRouter'
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
@@ -20,19 +21,33 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch()
 
+    const [checking, setChecking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     useEffect(() => {
         onAuthStateChanged(auth, (user ) => {
 
             if (user?.uid) {
-                dispatch(login(user.uid, user.displayName))
+                dispatch(login(user.uid, user.displayName));
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false)
             }
-                
+            
+            setChecking(false)
                 
             
         })
         
-    })
+    },[auth,dispatch,setChecking])
 
+    if (checking) {
+        return (
+            <div className="auth__loading">
+                <ReactLoading type={'spin'} color={"#5c62c5"} height={300} width={375} />
+                </div>
+        )
+    }
     
     return (
 
