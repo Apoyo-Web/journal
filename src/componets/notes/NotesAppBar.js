f94fa8a9d351e8b0c163e3912131eff5af5029e2
Apoyo-Web <mpienso@gmail.com>
2021-10-04 18:39:41 +0200

    Date on NotesAppBar

--- src/componets/notes/NotesAppBar.js
+++ src/componets/notes/NotesAppBar.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { startSaveNote } from '../../actions/notes'
+import moment from 'moment'
+import 'moment/locale/es'
 
 export const NotesAppBar = () => {
-
+    
     const dispatch = useDispatch()
-    const {active:note} = useSelector(state => state.notes)
+    const { active: note } = useSelector(state => state.notes)
+    const noteDate = moment(note.date).locale('es')
 
     const handleSave = () => {
         dispatch(startSaveNote(note))
     }
     return (
         <div className="notes__appbar">
-            <span>28 de agosto 2021</span>
+            <span>{ noteDate.format('dddd D MMMM')}</span>
             <div>
                 <button className="btn">
                     Picture
